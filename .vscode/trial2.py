3b0cb42f5bde9452fa7d4b9fbc5e6d9db36159a6
k156 <jm_91@live.co.kr>
2018-11-12 09:23:22 +0900

    blackjack draft

---
+++ .vscode/trial2.py
@@ -0,0 +1,7 @@
+class Number:
+    def iteration(self):
+        for i in range (1, 10):
+            print(i)
+            break
+
+Number.iteration()
--- temp.py
+++ temp.py
@@ -1,20 +1,6 @@
+class Number:
+    def iteration(self):
+        for i in range (1, 10)
+        print(i)
 
-    
-
-class Rec(Paral):
-    na=1
-
-class Square(Paral):
-    na =2
-
-
-
-paral = Paral("평행사변형")
-paral.multiply(5, 2)
-
-
-print(rec.multiply(3, 4))
-paral = Paral("평행")
-print(paral.multiply(2,5))
-rec = Rec("사각")
-
+Number.iteration()
