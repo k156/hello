d9cdaf4a5c5c0054d41f4a5fd237639093295d02
k156 <jm_91@live.co.kr>
2018-11-14 18:11:40 +0900

    module

--- module1.py
+++ module1.py
@@ -1,10 +1,17 @@
-import time
-import mysys
+import datetime
+import os
+import sys
+
+msg = sys.argv([1])
+
 
 def add_commit_push():
-    cd C:\workspace\hello
-    git add --all
-    git commit -am "{}-{} 강의".format(msg)
+    cd c:\workspace\hello
+    os.system("git add --all")
     if msg == false:
-        msg = (datetime.datetime.now.month, datetime.datetime.now.day)
-    git push -u origin master
+        input_msg = input("입력하고 싶은 메세지를 입력하거나 디폴트 메세지로 입력하려면 Enter를 누르세요.")
+        else:
+            os.system("git commit -am '{}-{} 강의'.format(msg)")
+            if input_msg == false:
+                input_msg = (datetime.datetime.now.month, datetime.datetime.now.day)
+                os.system("git push -u origin master")
