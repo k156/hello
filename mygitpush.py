65c4e1d2038061112377ff0cbbe44c3b13ab9eab
k156 <jm_91@live.co.kr>
2018-11-15 12:46:13 +0900

    2018-11-15 강의

--- mygitpush.py
+++ mygitpush.py
@@ -1,6 +1,7 @@
 import datetime
 import os, sys
 
+now = datetime.datetime.now()
 
 msg = ""
 if len(sys.argv) > 1:
@@ -11,11 +12,11 @@ def autogit():
     if msg == '':
         input_msg = input("입력하고 싶은 메세지를 입력하거나 디폴트 메세지로 입력하려면 Enter를 누르세요.")
         if input_msg == '':
-            auto_msg = (datetime.datetime.now.month, datetime.datetime.now.day)
-            os.system("git commit -am '{}-{} 강의'".format(auto_msg))
+            auto_msg = (now.strftime('%Y-%m-%d'))
+            os.system('git commit -am "{} 강의"'.format(auto_msg))
         else:
             msg_2 = input_msg
-            os.system("git commit -am '{}'".format(msg_2))
+            os.system('git commit -am "{}"'.format(msg_2))
     else:
         os.system('git commit -am "{}"'.format(msg))
     os.system("git push -u origin master")
