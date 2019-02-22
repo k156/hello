c57761a300291eb94277a1648c6ef1eb5a001fc9
k156 <jm_91@live.co.kr>
2019-02-22 18:06:35 +0900

    dd'

--- bigdata/pushdata.py
+++ bigdata/pushdata.py
@@ -8,10 +8,10 @@ if not client.check_table(DATABASE, TABLE):
     print("Create table {0}.{1}".format(DATABASE, TABLE), file=sys.stderr)
 
     client.create_table(DATABASE, TABLE, [
-        {'name': 'songno', 'type': 'string', 'description': 'song id'},
-        {'name': 'title', 'type': 'string', 'description': 'song title'},
-        {'name': 'albumid', 'type': 'string', 'description': 'album id'},
-    ])
+        {'name': 'songno', 'type': 'string', 'description': 'sub.id', 'abc'},
+    #     {'name': 'title', 'type': 'string', 'description': 'song title'},
+    #     {'name': 'albumid', 'type': 'string', 'description': 'album id'},
+    # ])
 ttt = [ {'songno': '222', 'title': '홍2', 'albumid': '121212121'} ]
 pushResult = client.push_rows(DATABASE, TABLE, ttt, insert_id_key='songno')
 print("Pushed Result is", pushResult)
